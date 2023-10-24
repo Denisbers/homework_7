9e5f1510575f2f0f3d14183efafc0d616813cdbd
Denisbers <denis16031984@gmail.com>
2023-10-24 23:50:46 +0300

    repo1

---
+++ app.js
@@ -0,0 +1,6 @@
+let digitFirst = +prompt('Enter first digit, please');
+let digitSecond = +prompt('Enter second digit, please');
+let digitThird = +prompt('Enter third digit, please');
+let average = (digitFirst + digitSecond + digitThird) / 3;
+
+alert(average)
